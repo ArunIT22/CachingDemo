0b7a256ed21e52f8222ffc258819a83af88ef1aa
arunit22 <arun.it.trainer@gmail.com>
2023-03-13 12:16:53 +0530

    Add project files.

---
+++ CachingDemo/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿using CachingDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System.Diagnostics;
+
+namespace CachingDemo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMemoryCache _memoryCache;
+
+        public ProductsController(ApplicationDbContext context, IMemoryCache memoryCache)
+        {
+            _context = context;
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            var watch = new Stopwatch();
+            Console.WriteLine("Fetching Product Details");
+            string cacheKey = "ProductList";
+
+            watch.Reset();
+            watch.Start();
+            if (_memoryCache.TryGetValue(cacheKey, out IEnumerable<Product> Products))
+            {
+                watch.Stop();
+                Console.WriteLine($"Product Details found in cache. Elapsed Time :{watch.ElapsedMilliseconds}");
+                return View(Products);
+            }
+            else
+            {
+                watch.Reset();
+                watch.Start();
+                var prd = _context.Products.ToList();
+                watch.Stop();
+                _memoryCache.Set(cacheKey, prd, new MemoryCacheEntryOptions
+                {
+                    SlidingExpiration = TimeSpan.FromSeconds(30),
+                    AbsoluteExpiration = DateTimeOffset.Now.AddHours(1)
+                });
+                Console.WriteLine($"Product Details not found in Cache. Fetching from Database. Elapsed Time :{watch.ElapsedMilliseconds}");
+                return View(prd);
+            }
+        }
+    }
+}
---
+++ CachingDemo/Models/ApplicationDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CachingDemo.Models
+{
+    public partial class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("products", "production");
+
+                entity.Property(e => e.ProductId).HasColumnName("product_id");
+
+                entity.Property(e => e.BrandId).HasColumnName("brand_id");
+
+                entity.Property(e => e.CategoryId).HasColumnName("category_id");
+
+                entity.Property(e => e.ListPrice)
+                    .HasColumnType("decimal(10, 2)")
+                    .HasColumnName("list_price");
+
+                entity.Property(e => e.ModelYear).HasColumnName("model_year");
+
+                entity.Property(e => e.ProductName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("product_name");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
